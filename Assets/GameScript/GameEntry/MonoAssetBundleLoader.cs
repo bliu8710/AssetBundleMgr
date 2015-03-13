98852554728d918f0ecf6cd1316ef9e6a33512ce
bliu8710 <bliu8710@gmail.com>
2015-03-13 19:06:59 +0800

    Load/UnLoad Button

--- Assets/GameScript/GameEntry/MonoAssetBundleLoader.cs
+++ Assets/GameScript/GameEntry/MonoAssetBundleLoader.cs
@@ -5,16 +5,44 @@ public class MonoAssetBundleLoader : BaseLoader
 {
 	public string assetBundleName = "data.unity3d";
 	public string assetName = "data";
+	private bool isLoadFinished = false;
 
 	// Use this for initialization
-	IEnumerator Start () {
-
+	IEnumerator Start ()
+	{
 		yield return StartCoroutine(Initialize() );
-
-		// Load asset.
-		yield return StartCoroutine(Load (assetBundleName, assetName) );
-
-		// Unload assetBundles.
-		AssetBundleManager.UnloadAssetBundle(assetBundleName);
 	}
+
+	public void Load()
+	{
+		Debug.Log("function: Load");
+
+		StartCoroutine(LoadAsset());
+
+		return;
+	}
+
+	public IEnumerator LoadAsset()
+	{
+		isLoadFinished = false;
+
+		Debug.Log("IEnumerator LoadAsset");
+		// Load asset.
+		yield return StartCoroutine(Load(assetBundleName, assetName));
+
+		isLoadFinished = true;
+		Debug.Log("IEnumerator isLoadFinished");
+	}
+
+	public void UnLoadAsset()
+	{
+		if (isLoadFinished)
+		{
+			// Unload assetBundles.
+			AssetBundleManager.UnloadAssetBundle(assetBundleName);
+			isLoadFinished = false;
+		}
+	}
+
+
 }
