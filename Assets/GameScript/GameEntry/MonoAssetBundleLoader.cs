c66a9c8fbf5afd3a9a3a8fa345e0d6da192d51c0
bliu8710 <bliu8710@gmail.com>
2015-03-13 16:14:13 +0800

    init demo

---
+++ Assets/GameScript/GameEntry/MonoAssetBundleLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonoAssetBundleLoader : BaseLoader
+{
+	public string assetBundleName = "data.unity3d";
+	public string assetName = "data";
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		yield return StartCoroutine(Initialize() );
+
+		// Load asset.
+		yield return StartCoroutine(Load (assetBundleName, assetName) );
+
+		// Unload assetBundles.
+		AssetBundleManager.UnloadAssetBundle(assetBundleName);
+	}
+}
